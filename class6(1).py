1322e0481e18ac9a4c6f24eef85ed081c480cf16
AaryaSandesh <aaryasandesh2601@gamil.com>
2025-02-24 22:56:33 +0530

    Updated code

--- class6(1).py
+++ class6(1).py
@@ -7,3 +7,35 @@ matrix=[
 print(matrix)
 
 print(matrix[1][0])
+
+# Question 1
+# Sum of all numbers :
+b=int(input("Size of list:"))
+li=[]
+for i in range (b) :
+    c=int(input("Enter number:"))
+    li.append(c)
+
+sum=0
+for i in li:
+    sum=sum+i
+print(sum)
+
+# Avg of List
+avg=sum/b
+print(avg)
+
+h=int(input("Size of list:"))
+list=[]
+for i in range (h) :
+    m=int(input("Enter number:"))
+    list.append(m)
+target=int(input("Enter target:"))
+
+flag=-1
+for i in range(len(list)) :
+    if list[i]==target :
+         flag=i
+         break
+    
+print(flag)
